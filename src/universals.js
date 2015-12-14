edd0d3264fc72d704311916dad6ff50d21a69624
rvhoyt@gmail.com <rvhoyt@gmail.com>
2015-12-14 12:31:08 -0500

    add persistent session

--- src/universals.js
+++ src/universals.js
@@ -1,2 +1,13 @@
 //Reset scope to 1 for every page, so next search doesn't break
 createCookie('SESSION_SCOPE', 1);
+
+
+//keep logged in
+$(document).ready(function() {
+  if (window.location.href.indexOf('top') > 0 && window.location.href.indexOf('patroninfo') > 0) {
+    setCookie('account', window.location.href, null, '/');
+  }
+  if (readCookie('account')) {
+    $('a[href$="/patroninfo"]').attr('href', readCookie('account'));
+  }
+});
