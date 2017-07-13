047ba0d560f8184acb0256174f30f661b1ce3812
Hoyt <rhoyt5@univ.fairfield.local>
2017-07-13 11:50:04 -0400

    fix login link

--- src/universals.js
+++ src/universals.js
@@ -5,7 +5,7 @@ createCookie('SESSION_SCOPE', 1);
 //keep logged in
 $(document).ready(function() {
   if (window.location.href.indexOf('patroninfo') > 0 && (window.location.href.indexOf('top') > 0 || window.location.href.indexOf('mylists') > 0)) {
-    setCookie('account', window.location.href, null, '/');
+    setCookie('account', window.location.href.replace('mylists', 'items'), null, '/');
   }
   if (readCookie('account')) {
     $('a[href$="/patroninfo"]').attr('href', readCookie('account'));
